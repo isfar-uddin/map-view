b97b8f71e394e55e2ae0b483ca3ab2f1cd0e1cca
isfar-uddin <isfarruett@gmail.com>
2018-02-01 20:45:37 +0600

    dependecy installed

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import MapView, {Marker} from 'react-native-maps'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.onRegionChange.bind(this)
+        this.state = {
+            region: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
+            markers:[]
+        }
+    }
+
+    onRegionChange(region) {
+        this.setState({region});
+    }
+
+    render() {
+        return (
+            <MapView
+                style={styles.map}
+                region={this.state.region}
+                onRegionChange={this.onRegionChange}
+            >
+                {this.state.markers.map(marker => (
+                    <Marker
+                        coordinate={marker.latlng}
+                        title={marker.title}
+                        description={marker.description}
+                    />
+                ))}
+            </MapView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 10,
+        bottom: 0
+    }
+
 });
